6ffa7353d4330fb1e645c3a629af79342ceac2c1
505 Computer A <zachauna@gmail.com>
2015-11-03 10:54:25 -0700

    -Start work on quick draw

---
+++ Fabulous Tank War/Assets/Script/QuickDraw.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class QuickDraw : MonoBehaviour {
+
+	public Button exclaim;
+
+	void Start () {
+	
+	}
+	void Update () {
+	
+	}
+}

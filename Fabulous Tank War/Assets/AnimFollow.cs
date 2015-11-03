e2a3a38055e4a9f2b7fc5fe0d852d65fd90b6280
Stephen Larsen <maxpendragon@gmail.com>
2015-11-03 15:13:58 -0700

    Started working on prototype thingy.

---
+++ Fabulous Tank War/Assets/AnimFollow.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimFollow : MonoBehaviour {
+
+	public bool lookAtPlyr;
+	public bool animPlay;
+	public Transform player;
+
+	// Use this for initialization
+	void Start () {
+		lookAtPlyr = true;
+		animPlay = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (lookAtPlyr == true)
+			transform.LookAt (player);
+		//if (animPlay == false)
+
+	}
+}

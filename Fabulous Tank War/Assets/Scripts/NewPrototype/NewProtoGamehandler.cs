e8883a3ff875a4aebb3ac3ddb562c7fceaac2763
Stephen <maxpendragon@gmail.com>
2015-11-17 09:58:18 -0700

    Started new, better, prototype.

---
+++ Fabulous Tank War/Assets/Scripts/NewPrototype/NewProtoGamehandler.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewProtoGamehandler : MonoBehaviour {
+
+    //x position for the three lanes on the runway: Left = -3, Center = 0, Right = 3
+
+    //
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Fabulous Tank War/Assets/Scripts/NewPrototype/PlayerReacts.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerReacts : MonoBehaviour {
+
+    //if button = swipeRight move player 1 lane to the right
+    //if button = swipeLeft move player 1 lane to the left
+    //if player in left lane do not show swipeLeft events
+    //if player in right lane do not show swipeRight events
+    
+    //if successful response to button then +points and show response
+    
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //RESPONSES
+    //
+    //-Beauty
+    //--> Style animations
+    //--> Moves Tank
+    //--> Success: Bonus points to beauty
+    //
+    //-Firepower
+    //--> Quickdraw event
+    //--> Fires at enemies
+    //--> Success: Bonus points to firepower
+    //
+    //-Durability
+    //--> Tap fast
+    //--> More taps in allotted time = +bonus points to durability
+}

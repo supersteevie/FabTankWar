12517ee996265f86ebd2ab31a3c36f601b05e2b5
505 Lab <zachauna@gmail.com>
2015-11-17 19:07:14 -0700

    -Finished Modular Quick Time Shape

--- Fabulous Tank War/Assets/Scripts/QuickTimePattern.cs
+++ Fabulous Tank War/Assets/Scripts/QuickTimePattern.cs
@@ -2,32 +2,60 @@
 using System.Collections;
 using System.Collections.Generic;
 
-public class QuickTimePattern : MonoBehaviour {
+public class QuickTimePattern : MonoBehaviour
+{
 
     //List of positions for mouse to detecr
     public List<Transform> patternList = new List<Transform>();
     //How close you must get to the point to detect
     public float radiusDetection;
     //For list of points
-    private Queue<Vector2> currentTemp = new Queue<Vector2>();
+    private Queue<Transform> currentTemp = new Queue<Transform>();
 
-    IEnumerator StartQuickTimePattern(float timer)
+    public bool isRunning = false;
+
+    public void StartQuickTimePattern(float timer)
     {
-        //Adds the list to queue
+        //Runs and clears the queue
+        currentTemp.Clear();
+        //Fills the queue
         foreach (Transform pos in patternList)
-            currentTemp.Enqueue(Camera.main.GetComponent<Camera>().WorldToScreenPoint(pos.position));
+        {
+            currentTemp.Enqueue(pos);
+            pos.GetComponent<MeshRenderer>().enabled = true;
+        }
+        //Starts the event
+        StartCoroutine(StartEvent(timer));
+    }
+
+    IEnumerator StartEvent(float timer)
+    {
+        //trigger event
+        isRunning = true;
         yield return new WaitForSeconds(timer);
+        isRunning = false;
     }
 
     void Update()
     {
-        //Stop once all points are hit
-        print(currentTemp.Count);
-        if (currentTemp.Count > 0)
-            if (Input.GetAxis("Fire1") > 0)  //Check if the button is pressed down
+        //detect positions and clear the queue
+        if (isRunning)
+            if (currentTemp.Count > 0)
             {
-                if (Vector2.Distance(currentTemp.Peek(), Input.mousePosition) <= radiusDetection)   //See if it within the desired radius
-                    currentTemp.Dequeue(); //Remove from queue once it is detected
+                if (Input.GetAxis("Fire1") > 0)
+                {
+                    if ((Vector2.Distance(Camera.main.GetComponent<Camera>().WorldToScreenPoint(currentTemp.Peek().position), Input.mousePosition)) <= radiusDetection)
+                    {
+                        currentTemp.Peek().GetComponent<MeshRenderer>().enabled = false;
+                        currentTemp.Dequeue();
+                    }
+                }
             }
-	}
+            else
+            {
+                //if they finished the shape end the coroutines
+                StopAllCoroutines();
+                isRunning = false;
+            }
+    }
 }

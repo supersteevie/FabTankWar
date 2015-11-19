37a827d2c52bb55f6e7dcbeac9ddce58b1157bbe
Kat Camp <kxmen1@aol.com>
2015-11-19 14:24:11 -0700

    Finished DataBase for now

--- Fabulous Tank War/Assets/Scripts/LoadInformation.cs
+++ Fabulous Tank War/Assets/Scripts/LoadInformation.cs
@@ -3,12 +3,20 @@ using System.Collections;
 
 public class LoadInformation  {
 
-	public static void LoadAllInfomation(){
+	public static void LoadAllInfomation(int Slot){
 
-		GameInformation.PlayerName = PlayerPrefs.GetString("PLAYERNAME");
-		GameInformation.StareRating = PlayerPrefs.GetInt ("STARRATING");
-		GameInformation.BeautyRating = PlayerPrefs.GetInt ("BEAUTYRATING");
-		GameInformation.FirePowerRating = PlayerPrefs.GetInt ("FIREPOWERRATING");
-		GameInformation.DurabilityRating = PlayerPrefs.GetInt ("DURABILITYRATING");
+		GameInformation.TurretID = PlayerPrefs.GetInt (Slot.ToString() + "TURRETID", 0);
+		GameInformation.HullID = PlayerPrefs.GetInt (Slot.ToString() + "HULLID", 0);
+		GameInformation.WheelsID = PlayerPrefs.GetInt (Slot.ToString() + "WHEELSID", 0);
+		GameInformation.PlayerName = PlayerPrefs.GetString(Slot.ToString() + "PLAYERNAME", "Player");
+		GameInformation.StareRating = PlayerPrefs.GetInt (Slot.ToString() + "STARRATING", 0);
+		GameInformation.BeautyRating = PlayerPrefs.GetInt (Slot.ToString() + "BEAUTYRATING", 0);
+		GameInformation.FirePowerRating = PlayerPrefs.GetInt (Slot.ToString() + "FIREPOWERRATING", 0);
+		GameInformation.DurabilityRating = PlayerPrefs.GetInt (Slot.ToString() + "DURABILITYRATING", 0);
 	}
+	//public static GameObject Tank(int playerID){
+
+		// return null;
+
+//	}
 }
---
+++ Fabulous Tank War/Assets/Scripts/SaveInformation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SaveInformation {
+
+	//Saveing Of Information
+
+	public static void GameInformationStore(int TurretID, int HullID, int WheelsID, int Slot, string PlayerName,int StarRating, int BeautyRating, int FirePowerRating, int DurabilityRating ){
+
+		PlayerPrefs.SetInt (Slot.ToString() + "TURRETID", TurretID);
+		PlayerPrefs.SetInt (Slot.ToString() + "HULLID", HullID);
+		PlayerPrefs.SetInt (Slot.ToString() + "WHEELSID", WheelsID);
+		PlayerPrefs.SetString(Slot.ToString() + "PLAYERNAME", PlayerName);
+		PlayerPrefs.SetInt (Slot.ToString() + "STARRATING", StarRating);
+		PlayerPrefs.SetInt (Slot.ToString() + "BEAUTYRATING",BeautyRating);
+		PlayerPrefs.SetInt (Slot.ToString() + "FIREPOWERRATING",FirePowerRating);
+		PlayerPrefs.SetInt (Slot.ToString() + "DURABILITYRATING", DurabilityRating);
+
+
+	}
+
+
+
+}

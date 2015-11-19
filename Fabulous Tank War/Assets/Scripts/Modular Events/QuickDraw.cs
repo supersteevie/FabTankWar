7732712968b9789b470ec24a362054b1cd629947
Zachary Thorpe <zachauna@gmail.com>
2015-11-19 15:59:09 -0700

    -Clean older scipts, quicktime events

---
+++ Fabulous Tank War/Assets/Scripts/Modular Events/QuickDraw.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class QuickDraw : MonoBehaviour
+{
+	public bool isRunning;
+
+	public void StartTimer (float timer)
+	{
+		StartCoroutine (StartEvent (timer));
+	}
+
+	IEnumerator StartEvent (float timer)
+	{
+		//trigger event
+		isRunning = true;
+		yield return new WaitForSeconds (timer);
+		isRunning = false;
+	}
+	
+	void Update ()
+	{
+		if (isRunning)
+			if (Input.GetAxis ("Fire1") > 0)
+				{
+					StopAllCoroutines ();
+					isRunning = false;
+					print ("Clicked in time");
+				}
+	}
+}

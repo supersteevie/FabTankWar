36b20c62234dea255ebac0285dcdc0cf5e1658fe
Stephen Larsen <Stephen.Larsen@uvu.edu>
2015-11-19 17:12:13 -0700

    Got coroutine for moving working.

--- Fabulous Tank War/Assets/Scripts/NewPrototype/NewProtoGamehandler.cs
+++ Fabulous Tank War/Assets/Scripts/NewPrototype/NewProtoGamehandler.cs
@@ -17,6 +17,8 @@ public class NewProtoGamehandler : MonoBehaviour {
     public GameObject playerObj;
     public GameObject towerObj;
 
+	public GameObject quickDrawUI;
+
     public float laneLeft;      // -3
     public float laneCenter;    // 0
     public float laneRight;     // 3
@@ -24,9 +26,18 @@ public class NewProtoGamehandler : MonoBehaviour {
     public Vector3 endPostion;
     public float moveSpeed;
 
+	private float timeBetweenEvents;
+	public float timeMin;
+	public float timeMax;
+
+	public float quickDrawTimer;
+	public float quickTapsTimer;
+	public float quickPatternTimer;
+
 	// Use this for initialization
 	void Start () {
-
+		StartCoroutine (BeginShow());
+		StartCoroutine (RunShow());
 
 	}
 	
@@ -35,15 +46,50 @@ public class NewProtoGamehandler : MonoBehaviour {
 	
 	}
 
-    IEnumerator BeginShow(GameObject thePlayer, Vector3 target)
+    IEnumerator BeginShow()
     {
-        while (Vector3.Distance(thePlayer.transform.position, target) > 0.05f)
+        while (Vector3.Distance(playerObj.transform.position, endPostion) > 0.05f)
         {
-            thePlayer.transform.position = Vector3.Lerp(thePlayer.transform.position, target, moveSpeed * Time.deltaTime);
+            playerObj.transform.position = Vector3.Lerp(playerObj.transform.position, endPostion, moveSpeed * Time.deltaTime);
+			endPostion.x = playerObj.transform.position.x;
 
             yield return null;
         }
     }
+
+	IEnumerator RunShow ()
+	{
+		while (Vector3.Distance(playerObj.transform.position, endPostion) > 0.05f)
+		{
+			timeBetweenEvents = Random.Range(timeMin, timeMax);
+			yield return new WaitForSeconds (timeBetweenEvents);
+			quickDrawUI.GetComponent<QuickDraw>().StartTimer(quickDrawTimer);
+			//quickTimeEvents();
+			yield return null;
+		}
+	}
+
+
+	/*
+	QuickTimeEvents QuicktimeSwitcher() {
+		int rand = Random.Range (0, 2);
+		quickTimeEvents = null;
+		switch (rand) {
+		case 0:
+			quickTimeEvents = quickDrawUI.GetComponent<QuickDraw>().StartTimer(quickDrawTimer);
+			break;
+		case 1:
+			quickTimeEvents = QuickTaps.StartTimer(quickTapsTimer);
+			break;
+		case 2:
+			quickTimeEvents = QuickTimePattern.StartQuickTimePattern(quickPatternTimer);
+			break;
+		}
+		
+		return quickTimeEvents;
+
+	}*/
+
 
     /*
     void QuicktimeSwitcher(int num)

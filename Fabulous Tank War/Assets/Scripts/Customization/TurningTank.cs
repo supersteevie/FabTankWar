abc7de4f72217867bc6adca71e8780956cb76960
Kat Camp <kxmen1@aol.com>
2015-12-03 12:11:44 -0700

    Fished Up fixing customize page.

---
+++ Fabulous Tank War/Assets/Scripts/Customization/TurningTank.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurningTank : MonoBehaviour {
+
+	private float turningSpeed = 2.5f;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (Vector3.up, turningSpeed * Time.deltaTime);
+	}
+}

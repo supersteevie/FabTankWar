45bfa9e2f5a37e0270e1b1019778414ff0eef53d
505 Lab <zachauna@gmail.com>
2015-12-10 09:58:24 -0700

    Functional master

---
+++ Fabulous Tank War/Assets/Scripts/Title Page/DontNoDestroy.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DontNoDestroy : MonoBehaviour {
+
+	private bool isOn = true;
+	// Use this for initialization
+	void Start () {
+		DontDestroyOnLoad(this.gameObject);
+	}
+}
